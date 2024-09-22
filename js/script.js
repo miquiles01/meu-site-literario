cd9b7dbed4ccfc0be0da78ce6963c1e10d93f1dc
miquiles01 <tatimiquiles@gmail.com>
2024-09-22 14:46:38 -0300

    navbar e grid resposivos

--- js/script.js
+++ js/script.js
@@ -2,3 +2,10 @@ document.querySelector('.contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Mensagem enviada com sucesso!');
 });
+
+const mobileMenu = document.getElementById("mobile-menu");
+const navbarItems = document.querySelector(".navbar-items");
+
+mobileMenu.addEventListener("click", () => {
+    navbarItems.classList.toggle("active");
+});
