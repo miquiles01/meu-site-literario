851ed7b8dd9b75df7b1c65fdf070246cbd10ed08
miquiles01 <tatimiquiles@gmail.com>
2024-09-22 15:32:37 -0300

    ajuste js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,3 @@ document.querySelector('.contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Mensagem enviada com sucesso!');
 });
-
-const mobileMenu = document.getElementById("mobile-menu");
-const navbarItems = document.querySelector(".navbar-items");
-
-mobileMenu.addEventListener("click", () => {
-    navbarItems.classList.toggle("active");
-});
